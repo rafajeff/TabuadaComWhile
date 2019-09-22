88b8ccb05270159b2a9386fe4eae593c27dfa710
rafael jeferson <rafajeferson@hotmail.com>
2019-09-22 16:21:20 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = 0;
+
+            Console.WriteLine("Digite um número do qual quer saber a tabuada");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            while (i < 11 ) 
+            {
+                Console.WriteLine(i*n);
+                i++;
+            }
+            
+        }
+    }
+}
